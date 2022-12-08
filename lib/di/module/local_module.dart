878c964a4f05512cf8f50c2e98753d1968924816
SON <son@SONs-Mac-mini.local>
2022-12-08 17:37:08 +0700

    boilerplate

---
+++ lib/di/module/local_module.dart
@@ -0,0 +1,51 @@
+// ignore_for_file: depend_on_referenced_packages
+
+import 'dart:async';
+
+import 'package:path_provider/path_provider.dart';
+import 'package:sembast/sembast.dart';
+import 'package:sembast/sembast_io.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../../data/local/constants/db_constants.dart';
+import 'package:path/path.dart' as p;
+import '../../utils/encryption/xxtea.dart';
+
+abstract class LocalModule {
+
+
+  /// A singleton preference provider.
+  ///
+  /// Calling it multiple times will return the same instance.
+  static Future<SharedPreferences> provideSharedPreferences() {
+    return SharedPreferences.getInstance();
+  }
+
+  /// A singleton database provider.
+  ///
+  /// Calling it multiple times will return the same instance.
+  static Future<Database> provideDatabase() async {
+    // Key for encryption
+    var encryptionKey = "";
+
+    // Get a platform-specific directory where persistent app data can be stored
+    final appDocumentDir = await getApplicationDocumentsDirectory();
+
+    // Path with the form: /platform-specific-directory/demo.db
+    final dbPath = p.join(appDocumentDir.path, DBConstants.DB_NAME);
+
+    // Check to see if encryption is set, then provide codec
+    // else init normal db with path
+    Database database;
+    if (encryptionKey.isNotEmpty) {
+      // Initialize the encryption codec with a user password
+      var codec = getXXTeaCodec(password: encryptionKey);
+      database = await databaseFactoryIo.openDatabase(dbPath, codec: codec);
+    } else {
+      database = await databaseFactoryIo.openDatabase(dbPath);
+    }
+
+    // Return database instance
+    return database;
+  }
+}
---
+++ lib/stores/post/post_store.dart
@@ -0,0 +1,50 @@
+import 'package:mobx/mobx.dart';
+
+import '../../data/repository.dart';
+import '../../models/post/post_list.dart';
+import '../../utils/dio/dio_error_util.dart';
+import '../error/error_store.dart';
+
+part 'post_store.g.dart';
+
+class PostStore = _PostStore with _$PostStore;
+
+abstract class _PostStore with Store {
+  // repository instance
+  late final Repository _repository;
+
+  // store for handling errors
+  final ErrorStore errorStore = ErrorStore();
+
+  // constructor:---------------------------------------------------------------
+  _PostStore(Repository repository) : _repository = repository;
+
+  // store variables:-----------------------------------------------------------
+  static ObservableFuture<PostList?> emptyPostResponse =
+      ObservableFuture.value(null);
+
+  @observable
+  ObservableFuture<PostList?> fetchPostsFuture = ObservableFuture<PostList?>(emptyPostResponse);
+
+  @observable
+  PostList? postList;
+
+  @observable
+  bool success = false;
+
+  @computed
+  bool get loading => fetchPostsFuture.status == FutureStatus.pending;
+
+  // actions:-------------------------------------------------------------------
+  @action
+  Future getPosts() async {
+    final future = _repository.getPosts();
+    fetchPostsFuture = ObservableFuture(future);
+
+    future.then((postList) {
+      this.postList = postList;
+    }).catchError((error) {
+      errorStore.errorMessage = DioErrorUtil.handleError(error);
+    });
+  }
+}
